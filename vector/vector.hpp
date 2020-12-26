075375c437d8ad3be1a686872cff78eba7c029d6
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2020-12-26 20:37:14 +0100

    Building the core ...

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: pablo <pablo@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/26 16:37:12 by pablo             #+#    #+#             */
-/*   Updated: 2020/12/26 17:44:36 by pablo            ###   ########lyon.fr   */
+/*   Updated: 2020/12/26 20:36:42 by pablo            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 */
 
 # include <allocator.hpp>
+# include "LegacyRandomAccessIterator.hpp"
 
 // A vector works like malloc: pre-allocates mem blocks to handle future grow (MUST DO IT)
 // 		for that check: capacity() and reserve()
@@ -39,17 +40,80 @@ namespace ft
 		typedef const T*		const_pointer;
 		// TO DO: iterators
 
+		private:
+	
+		/* Core */
+
+		Allocator				memory;
+		size_type				max_size;
+		pointer					array;
+		size_type				curr_size;
+
+		/*
+		** Like a kind of realloc, if the current size is greather than the
+		** max size (capacity) it will create a new array and perform a deep copy
+		** of all the current elements stored in the current array.
+		**
+		** Note: Need to be called each time the curr_size is updated.
+		*/
+		void					array_reserve()
+		{
+			if (curr_size > max_size)
+			{
+				try {
+					pointer	new_array = memory.allocate(curr_size);
+				} catch (const std::bad_alloc& e) { if (array) this->~vector(); throw ; }
+				for (size_type i = 0 ; i < curr_size ; i++)
+				{
+					// check if i m allowed to play God with std::move
+					new_array = std::move(array + i);
+					memory.destroy(array + i);
+				}
+				memory.deallocate(array);
+				array = new_array;
+			}
+		}
+
+		/*
+		** Duplicates the max_size and call array_reserve to perform the reallocation.
+		*/
+		void				array_resize()
+		{
+			if (curr_size > max_size)
+			{
+				curr_size = max_size * 2;
+				array_reserve();
+			}
+		}
+
+		public:
+
 		/* Member functions */
 
 			/* Constructor */
-		vector();
-		explicit vector(const Allocator& alloc);
-		explicit vector(size_type count, const_reference value = value_type(), const Allocator& alloc = Allocator());
+		explicit vector(const Allocator& alloc = nullptr) : memory(alloc), max_size(0ul), array(nullptr), curr_size(0ul) { }
+
+		explicit vector(size_type count, const_reference value = value_type(), const Allocator& alloc = Allocator())
+		: memory(alloc), max_size(count), curr_size(cout)
+		{
+			try {
+				array = memory.allocate(max_size);
+			} catch (const std::bad_alloc& e) { throw ; }
+			for (size_type i = 0 ; i < max_size)
+				memory.construct(array + i, value);
+		}
+
 		template <class InputIt>
-		vector(InputIt first, InputIt last, const Allocator& = Allocator());
+		vector(InputIt first, InputIt last, const Allocator& alloc = Allocator())
+		: memory(alloc), max_size(0ul), array(nullptr), curr_size(0ul) { assign(first, last); }
 
 			/* Destructor */
-		~vector();
+		~vector()
+		{
+			for (size_type i = 0 ; i < curr_size ; i++)
+				memory.destroy(array + i);
+			memory.deallocate(array, max_size);
+		}
 
 			/* Operator= */
 		vector&		operator=(const vector& other);
@@ -110,7 +174,7 @@ namespace ft
 			/* insert */
 		// to do
 
-			/* ease */
+			/* erase */
 		// to do
 
 			/* Push_back */
@@ -137,4 +201,181 @@ namespace ft
 		bool				operator>=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
 		
 	};
+
+	/* Operator= */
+	template <class T, class Allocator>
+	vector<T, Allocator>&	vector<T, Allocator>::operator=(const vector<T, Allocator>& other)
+	{
+		
+	}
+
+	/* Assign */
+	template <class T, class Allocator>
+	void					vector<T, Allocator>::assign(size_type count, const reference value)
+	{
+		
+	}
+
+	/* Assign */
+	template <class T, class Allocator>
+	template <class InputIt>
+	void					vector<T, Allocator>::assign(InputIt first, InputIt last)
+	{
+		
+	}
+
+	/* At */
+	template <class T, class Allocator>
+	vector<T, Allocator>::reference				vector<T, Allocator>::at(size_type pos)
+	{
+		
+	}
+
+	/* At */
+	template <class T, class Allocator>
+	vector<T, Allocator>::const_reference		vector<T, Allocator>::at(size_type pos) const
+	{
+		
+	}
+
+	/* Front */
+	template <class T, class Allocator>
+	vector<T, Allocator>::reference				vector<T, Allocator>::operator[](size_type pos)
+	{
+		
+	}
+
+	/* Front */
+	template <class T, class Allocator>
+	vector<T, Allocator>::const_reference		vector<T, Allocator>::operator[](size_type pos) const
+	{
+		
+	}
+	
+	/* Back */
+	template <class T, class Allocator>
+	vector<T, Allocator>::reference				vector<T, Allocator>::back()
+	{
+		
+	}
+
+	/* Back */
+	template <class T, class Allocator>
+	vector<T, Allocator>::const_reference		vector<T, Allocator>::back() const
+	{
+		
+	}
+
+	// TO DO begin
+
+	// TO DO end
+
+	/* Empty*/
+	template <class T, class Allocator>
+	bool										vector<T, Allocator>::empty() const
+	{
+		
+	}
+
+	/* Size */
+	template <class T, class Allocator>
+	vector<T, Allocator>::size_type				vector<T, Allocator>::size() const
+	{
+
+	}
+
+	/* Max size */
+	template <class T, class Allocator>
+	vector<T, Allocator>::size_type				vector<T, Allocator>::max_size() const
+	{
+
+	}
+
+	/* Reserve */
+	template <class T, class Allocator>
+	void										vector<T, Allocator>::reserve(size_type new_cap)
+	{
+		
+	}
+
+	/* Capacity */
+	template <class T, class Allocator>
+	vector<T, Allocator>::size_type				vector<T, Allocator>::capacity() const
+	{
+		
+	}
+
+	/* Clear */
+	template <class T, class Allocator>
+	void										vector<T, Allocator>::clear()
+	{
+		
+	}
+
+	/* Push back */
+	template <class T, class Allocator>
+	void										vector<T, Allocator>::push_back(const_reference value)
+	{
+		
+	}
+
+	/* Pop back */
+	template <class T, class Allocator>
+	void										vector<T, Allocator>::pop_back()
+	{
+		
+	}
+
+	/* Resize */
+	template <class T, class Allocator>
+	void										vector<T, Allocator>::resize(size_type count, value_type value = value_type())
+	{
+		
+	}
+
+	/* Booleans */
+
+	template <class T, class Alloc>
+	bool				operator==(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+	{
+		if (lhs.size() == rhs.size())
+		{
+			for (auto i = 0 ; i < lhs.size() < i++)
+				if (lhs.operator[](i) != rhs.operator[](i))
+					return (false);
+			return (true);
+		}
+		return (false);
+	}
+	
+	template <class T, class Alloc>
+	bool				operator!=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+	{
+		return (!operator==(lhs, rhs));
+	}
+
+	template <class T, class Alloc>
+	bool				operator<(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+	{
+		// think about it
+	}
+
+	template <class T, class Alloc>
+	bool				operator<=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+	{
+		return (operator==(lhs, rhs) || operator<(lhs, rhs));
+	}
+
+	template <class T, class Alloc>
+	bool				operator>(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+	{
+		return (!operator<=(lhs, rhs));
+	}
+
+	template <class T, class Alloc>
+	bool				operator>=(const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs)
+	{
+		return (!operator<(lhs, rhs));
+	}
 };
+
